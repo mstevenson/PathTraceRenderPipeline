3c8845786e11e945454ea04c1adffc171f51b304
Michael Stevenson <michael@mstevenson.net>
2018-10-10 07:35:43 -0700

    Init

---
+++ Assets/Scripts/PathTracerPipeline.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+[ExecuteInEditMode]
+public class PathTracerPipeline : RenderPipelineAsset
+{
+    [SerializeField] private ComputeShader _shader;
+    
+#if UNITY_EDITOR
+    // Call to create a simple pipeline
+    [UnityEditor.MenuItem("Create/Path Tracer Pipeline")]
+    private static void CreateBasicAssetPipeline()
+    {
+        var instance = CreateInstance<PathTracerPipeline>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/PathTracerPipeline.asset");
+    }
+#endif
+    
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new PathTracerPipelineInstance(_shader);
+    }
+}
---
+++ Assets/Scripts/PathTracerPipelineInstance.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+public class PathTracerPipelineInstance : RenderPipeline
+{
+    private readonly ComputeShader _shader;
+    private readonly int _width;
+    private readonly int _height;
+    
+    private const string KernelName = "CSMain";
+    
+    public PathTracerPipelineInstance(ComputeShader shader)
+    {
+        _shader = shader;
+        _width = Screen.width;
+        _height = Screen.height;
+    }
+
+    public override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        base.Render(context, cameras);
+
+        var cmd = new CommandBuffer();
+        cmd.ClearRenderTarget(true, true, Color.yellow);
+        
+        if (_shader != null && _shader.HasKernel(KernelName))
+        {
+            int index = _shader.FindKernel("CSMain");
+            var target = new RenderTargetIdentifier(BuiltinRenderTextureType.CurrentActive);
+            cmd.SetComputeTextureParam(_shader, index, "Result", target);
+            cmd.DispatchCompute(_shader, index, _width, _height, 1);
+        }
+        
+        context.ExecuteCommandBuffer(cmd);
+        
+        context.Submit();
+    }
+}
